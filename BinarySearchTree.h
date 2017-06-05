b19882917c761f8507eb1dafc0ca537c80058c39
Alexander McNulty <Alexander.mcnulty92@gmail.com>
2017-06-05 04:48:48 -0700

    finished lab4

---
+++ BinarySearchTree.h
@@ -0,0 +1,432 @@
+/*
+THIS PROGRAM IS A SIMPLE BINARY SEARCH TREE
+
+EXPOSED BINARY SEARCH TREE CLASS FUNCTIONS
+
+void printPostOrderTraverse(std::ofstream &writeFile)		- print post order traversal to file
+void printBreadthFirstTraverse(std::ofstream &writeFile)	- print breadth first traversal to file
+void breadth_firstTraversal()								- print breadth first traversal to screen	
+bool searchForValue(T value)								- find node in tree
+void searchAndModify(T findThis)							- find and replace node value in tree *will change tree structure*
+bool deleteValue(T value)									- delete node from tree *will change tree structure*
+void addValue(T value)										- add node to tree *if data values are the same, the root will be saved to the left*
+
+*/
+
+#ifndef BINARYSEARCHTREE_H
+#define BINARYSEARCHTREE_H
+
+#include<fstream>
+#include<iostream>
+#include"QueueADT.h"
+
+template <class T>
+class DualLinkDataNode
+{
+public:
+	T data;
+	DualLinkDataNode<T> *leftBranch;
+	DualLinkDataNode<T> *rightBranch;
+
+	// defualt constructor
+	DualLinkDataNode()
+	{
+		leftBranch = nullptr;
+		rightBranch = nullptr;
+	}
+
+	// constructor
+	DualLinkDataNode(T value)
+	{
+		data = value;
+		leftBranch = nullptr;
+		rightBranch = nullptr;
+	}
+};
+
+template <class T>
+class BinarySearchTree
+{
+private:
+	DualLinkDataNode<T> *rootNode;
+
+protected:
+	//  add nodes to the tree using a binary search traversal, and is called by addNode() function
+	//  this function is access protected from anything outside the class for safety
+	DualLinkDataNode<T> *addInOrder(DualLinkDataNode<T> *subTreePtr, DualLinkDataNode<T> *newNode)
+	{
+		// if the current sub-tree is empty, return the address of the new node
+		if (subTreePtr == nullptr)
+		{
+			return newNode;
+		}
+
+		else
+		{
+			// if the sub-tree's data is greater than or equal to new node's data, send new node down the left branch
+			if (newNode->data <= subTreePtr->data)
+			{
+				subTreePtr->leftBranch = addInOrder(subTreePtr->leftBranch, newNode);
+			}
+
+			else
+			{
+				subTreePtr->rightBranch = addInOrder(subTreePtr->rightBranch, newNode);
+			}
+
+			return subTreePtr;
+		}
+	}
+
+	// Removes the given target value from the tree while maintaining a binary search tree.
+	DualLinkDataNode<T> *removeValue(DualLinkDataNode<T> *subTreePtr, const T findThis, bool &successBoolean)
+	{
+		DualLinkDataNode<T> *tempPtr = nullptr;
+		// if the value was not found, terminate the removeValue() recursion, and return a false for success
+		if (subTreePtr == nullptr)
+		{
+			successBoolean = false;
+			return nullptr;
+		}
+
+		// if the value was found, send this node's address to removeNode() function and set success to true
+		else if (subTreePtr->data == findThis)
+		{
+			// Item is in the root of some subtree
+			subTreePtr = removeNode(subTreePtr);
+			successBoolean = true;
+			return subTreePtr;
+		}
+
+		else if (subTreePtr->data > findThis)
+		{
+			// Search the left subtree
+			tempPtr = removeValue(subTreePtr->leftBranch, findThis, successBoolean);
+			subTreePtr->leftBranch = tempPtr;
+			return subTreePtr;
+		}
+
+		else
+		{
+			// Search the right subtree
+			tempPtr = removeValue(subTreePtr->rightBranch, findThis, successBoolean);
+			subTreePtr->rightBranch = tempPtr;
+			return subTreePtr;
+		}
+	}
+
+	// Removes the item in the node, N, to which nodePtr points.
+	// Returns a pointer to the node at this tree location after node N is deleted.
+	DualLinkDataNode<T> *removeNode(DualLinkDataNode<T> *currentNode)
+	{
+		DualLinkDataNode<T> * tempPtr = nullptr;
+
+		// if the sub-tree has no left or right branches, delete the node and return a nullptr
+		if (currentNode->leftBranch == nullptr && currentNode->rightBranch == nullptr)
+		{
+			delete currentNode;
+			currentNode = nullptr;
+			return nullptr;
+		}
+
+		// if the sub-tree has only one branch
+		else if ((currentNode->leftBranch != nullptr && currentNode->rightBranch == nullptr) || (currentNode->rightBranch != nullptr && currentNode->leftBranch == nullptr))
+		{
+			// store currnetNode's address in tempPtr
+			tempPtr = currentNode;
+
+			//  if the sub-tree's path is only on the left branch, make subTreePtr point to the left branch
+			if (currentNode->leftBranch != nullptr && currentNode->rightBranch == nullptr)
+			{
+				currentNode = currentNode->leftBranch;
+				delete tempPtr;
+				tempPtr = nullptr;
+				return currentNode;
+			}
+
+			//  else, the path is only the right branch
+			else
+			{
+				currentNode = currentNode->rightBranch;
+				delete tempPtr;
+				tempPtr = nullptr;
+				return currentNode;
+			}
+		}
+
+		// else, the sub-tree has two branches
+		else
+		{
+			// Find the inorder successor of the entry in N: it is in the left subtree rooted at N’s right child
+			tempPtr = removeLeftMostNode(currentNode->rightBranch, currentNode->data);  // data is referenced in removeleftnode function
+			currentNode->rightBranch = tempPtr;
+			currentNode->data = currentNode->data;  // because the data was referenced, it was changed
+			return currentNode;
+		}
+	}
+
+	// Removes the leftmost node in the left subtree of the node pointed to by nodePtr.
+	// Sets inorderSuccessor to the value in this node.
+	// Returns a pointer to the revised subtree.
+	DualLinkDataNode<T> *removeLeftMostNode(DualLinkDataNode<T> *nodePtr, T &inorderSuccesssor)
+	{
+
+		if (nodePtr->leftBranch == nullptr)
+		{
+			// This is the node you want; it has no left child, but it might have a right subtree
+			inorderSuccesssor = nodePtr->data;
+			return removeNode(nodePtr);
+		}
+
+		else
+		{
+			return removeLeftMostNode(nodePtr->leftBranch, inorderSuccesssor);
+		}
+
+	}
+
+	//  Recursive function to search the tree for a specific value
+	DualLinkDataNode<T> *searchTree(const T findThis, DualLinkDataNode<T> *root, bool &successBoolean)
+	{
+		// if the value was not found, return false
+		if (root == nullptr)
+		{
+			successBoolean = false;
+			return nullptr;
+		}
+		// if the value is found, return true and unwind the recursion
+		else if (root->data == findThis)
+		{
+			successBoolean = true;
+			return root;
+		}
+		// if the search value is less than or equal to the data stored in the node, use recursion to travel down the left branch
+		else if (findThis <= root->data)
+		{
+			//return searchTree(value, root->leftBranch);
+			return searchTree(findThis, root->leftBranch, successBoolean);
+		}
+		// else travel down the right branch
+		else
+		{
+			//return searchTree(findThis, root->rightBranch);
+			return searchTree(findThis, root->rightBranch, successBoolean);
+		}
+	}
+
+	//  Use a recursion to travel down the left branches until a dead end is found
+	//  Then travel down the right branch until a dead is found
+	//  Once both paths are checked, print out the data in the node at the end of the branch
+	//  Also this function is protected from access outside of the class
+	void post_orderTraversal(DualLinkDataNode<T> *currentRoot, std::ofstream &writeFile)
+	{
+		// if the currentRoot is null, that means this branch's patch is at the end
+		if (currentRoot == nullptr)
+		{
+			return;
+		}
+		else
+		{
+			post_orderTraversal(currentRoot->leftBranch, writeFile); // check the left branch for a valid path
+			post_orderTraversal(currentRoot->rightBranch, writeFile); // check the right branch for a valid path
+			writeFile << currentRoot->data << std::endl; // when both branches are nullptr's, print out the current node's data
+		}
+	}
+
+public:
+	//------------------------------------------------------------
+	// Constructors and Destructors:
+	//------------------------------------------------------------
+
+	BinarySearchTree()
+	{
+		rootNode = nullptr;
+	}
+
+	~BinarySearchTree()
+	{
+		// destructor
+		// remove all the dynamically created nodes
+	}
+
+	//------------------------------------------------------------
+	// Public Methods:
+	//------------------------------------------------------------
+
+	// another way to add nodes to the binary tree, based on the book's design
+	void addValue(T value)
+	{
+		DualLinkDataNode<T> *newNode = new DualLinkDataNode<T>(value);
+
+		//  if the tree has no items, set the new node as the root node
+		if (!rootNode)
+		{
+			rootNode = newNode;
+		}
+
+		//  else, process the node using a binary search to find the correct location for the value
+		else
+		{
+			rootNode = addInOrder(rootNode, newNode);
+		}
+	}
+
+	//  the public remove node function
+	//  removes the node holding the passed value, if the node is in the tree
+	//  returns true if the node was removed, or returns false
+	bool deleteValue(T value)
+	{
+		bool status;  // store the result of the remove node recursion
+
+		//  if the tree has no items, print out the error message
+		if (!rootNode)
+		{
+			std::cout << "error: the tree has no items";
+			return false;
+		}
+
+		// else, call the protected removeValue() function
+		else
+		{
+			removeValue(rootNode, value, status);
+		}
+		return status;
+	}
+
+	bool searchForValue(T value)
+	{
+		bool status;
+
+		if (!rootNode)
+		{
+			return false;
+		}
+		else if (rootNode->data == value)
+		{
+			return true;
+		}
+		// if the search value is less than or equal to the data in rootNode, travel down the left branch
+		else if (value <= rootNode->data)
+		{
+			//return searchTree(value, rootNode->leftBranch);
+			searchTree(value, rootNode->leftBranch, status);
+
+		}
+		// else, travel down the right branch
+		else
+		{
+			//return searchTree(value, rootNode->rightBranch);
+			searchTree(value, rootNode->rightBranch, status);
+		}
+		return status;
+	}
+
+	void searchAndModify(T findThis)
+	{
+		// this is a two step process, first step is search for the value
+		// if the value is found, delete the node
+		// next step is insert a new node with a new value into the tree
+		// thus perserving the tree balance
+		T newValue;
+		bool status;
+		DualLinkDataNode<T> *nodeWithSeachValue = nullptr;
+
+		// search for a value, and if found, modify the node's data member
+		if (!rootNode)
+		{
+			std::cout << "error: tree is empty";
+			return;
+		}
+		else
+		{
+			status = deleteValue(findThis);
+		}
+		if (status == true)
+		{
+			std::cout << "current value: " << findThis << std::endl;
+			std::cout << "enter a new value: ";
+			std::cin >> newValue;
+			addValue(newValue);
+		}
+		else
+		{
+			std::cout << "value: " << findThis << " was not found";
+		}
+	}
+
+	//  breadth - first traversal mechanism - PRINTS TO TERMINAL
+	void breadth_firstTraversal()
+	{
+		if (rootNode == nullptr)
+		{
+			return;
+		}
+
+		else
+		{
+			QueueADT<DualLinkDataNode<T>*> Q;
+			Q.enQueue(rootNode);
+			//while there is at least one discovered node
+			while (!Q.isQueueEmpty())
+			{
+				DualLinkDataNode<T>* current = Q.getFront();
+				std::cout << current->data << " ";
+				if (current->leftBranch != nullptr)
+				{
+					Q.enQueue(current->leftBranch);
+				}
+				if (current->rightBranch != nullptr)
+				{
+					Q.enQueue(current->rightBranch);
+				}
+				Q.deQueue(); // remove the element at the front
+			}
+		}
+	}
+
+	//  breadth - first traversal mechanism - PRINTS TO FILE
+	void printBreadthFirstTraverse(std::ofstream &writeFile)
+	{
+		if (rootNode == nullptr)
+		{
+			return;
+		}
+
+		else
+		{
+			QueueADT<DualLinkDataNode<T>*> Q;
+			Q.enQueue(rootNode);
+			//while there is at least one discovered node
+			while (!Q.isQueueEmpty())
+			{
+				DualLinkDataNode<T>* current = Q.getFront();
+				writeFile << current->data << std::endl;
+				if (current->leftBranch != nullptr)
+				{
+					Q.enQueue(current->leftBranch);
+				}
+				if (current->rightBranch != nullptr)
+				{
+					Q.enQueue(current->rightBranch);
+				}
+				Q.deQueue(); // remove the element at the front
+			}
+		}
+	}
+
+
+
+	void printPostOrderTraverse(std::ofstream &writeFile)
+	{
+		if (!rootNode)
+		{
+			std::cout << "error: tree is empty";
+		}
+		else
+		{
+			post_orderTraversal(rootNode, writeFile);
+		}
+	}
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,233 +1,88 @@
+// 22C - Manish Goel
+// Lab 04: Binary Search Tree
+// Names: Alexander McNulty, Clifford Smith
+
+/*
+This program is a demonstration of binary search tree. 
+When run the program dataFile.txt in the CWD (current working directory)
+reads in the data then sorts and print them to two seperate files.
+
+Check the CWD for two files:
+	sortedBirthdates.txt - contains dates in breadth-first traversal 
+	sortedNames.txt		 - contains dates in post-order traversal
+
+This task is accomplished by the exposed behaviors in BinarySearchTree.h.
+BinarySearchTree.h requires LinkedList.h and QueueADT.h to be in CWD
+
+In this program we used:
+	addValue(string)
+	printPostOrderTraverse(ofstream)
+	printBreadthFirstTraverse(ofstream)
+
+See top of BinarySearchTree.h for additional methods
+
+*/
+
 #include<iostream>
 #include<string>
-#include"LinkedList.h"
-#include"QueueADT.h"
-#include"Currency.h"
+#include<fstream>
+#include"BinarySearchTree.h"
 
 using namespace std;
 
-/*
-	THIS PROGRAM IS A SIMPLE BINARY SEARCH TREE
-	//if data values are the same, the root will be saved to the left
-*/
+int main()
+{
+	// create and open the INPUT FILE - containing names, and birthdays
+	ifstream inputFile;
+	inputFile.open("./dataFile.txt");
 
-template <class T>
-struct BstNode {
-	T data;
-	BstNode* left;
-	BstNode* right;
-};
+	//create TWO SEPERATE TREES for holding birthdays or names
+	BinarySearchTree<string> firstAndLastNames;
+	BinarySearchTree<string> birthDates;
 
-template <class T>
-class BstRoot{
-private:
+	//READ, SORT, then ADD data to correct tree
+	string tempStorage;
+	while (!inputFile.eof())	
+	{	
 
-	BstNode<T>* root = NULL;
-
-public:
-
-	BstRoot(){
-	}
-
-	BstNode<T>* getNewNode(int data){
-		BstNode<T>* newNode = new BstNode<T>();
-		newNode->data = data;
-		newNode->left = NULL;
-		newNode->right = NULL;
-		return newNode;
-	}
-
-	void insert(int data) {
-		if (root == NULL){
-			root = getNewNode(data);
+		//READ the next line from the input file
+		getline(inputFile, tempStorage); 
+		//SORT ascii values for 0-9 to find DATES
+		if (tempStorage[0] == 48 ||
+			tempStorage[0] == 49 ||
+			tempStorage[0] == 50 ||
+			tempStorage[0] == 51 ||
+			tempStorage[0] == 52 ||
+			tempStorage[0] == 53 ||
+			tempStorage[0] == 54 ||
+			tempStorage[0] == 55 ||
+			tempStorage[0] == 56 ||
+			tempStorage[0] == 57 )
+		{
+			//if the line begins with a number ADD to BIRTHDAY TREE
+			birthDates.addValue(tempStorage);
 		}
-		//if data values are the same, the root will be saved to the left
-		else if (data <= root->data){
-			root->left = insert(data, root->left);
-		}
-		else {
-			root->right = insert(data, root->right);
+		else
+		{
+			//if not ADD to NAMES TREE
+			firstAndLastNames.addValue(tempStorage);
 		}
 	}
-	BstNode<T>* insert(int data, BstNode<T>* root) {
-		if (root == NULL){
-			root = getNewNode(data);
-		}
-		//if data values are the same, the root will be saved to the left
-		else if (data <= root->data){
-			root->left = insert(data, root->left);
-		}
-		else {
-			root->right = insert(data, root->right);
-		}
-		return root;
-	}
+	inputFile.close();
 
-	bool search(int data) {
-		if (root == NULL) return false;
-		else if (root->data == data) return true;
-		else if (data <= root->data) return search(data, root->left);
-		else return search(data, root->right);
-	}
-	bool search(int data, BstNode<T>* root) {
-		if (root == NULL) return false;
-		else if (root->data == data) return true;
-		else if (data <= root->data) return search(data, root->left);
-		else return search(data, root->right);
-	}
+	// create and open output file for names
+	// print the BST of names in POSTORDER to output file
+	ofstream namesOutputFile;
+	namesOutputFile.open("./sortedNames.txt");
+	firstAndLastNames.printPostOrderTraverse(namesOutputFile);
+	namesOutputFile.close();
 
-
-	void levelOrder() {
-		if (root == NULL) return;
-		
-		QueueADT<BstNode<T>*> Q;
-		Q.enQueue(root);
-		//while there is at least one discovered node
-		while (!Q.isQueueEmpty()){
-			BstNode<T>* current = Q.getFront();
-			cout << current->data << " ";
-			if (current->left != NULL) Q.enQueue(current->left);
-			if (current->right != NULL) Q.enQueue(current->right);
-			Q.deQueue(); // remove the element at the front
-		}
-	}
-
-};
-
-
-int main(){
-	BstRoot<int> root; //pointer stores the address of root node
-	root.insert(12);
-	root.insert(1);
-	root.insert(2);
-	root.insert(15);
-	root.insert(2);
-	root.insert(4);
-	root.insert(100);
-
-	root.levelOrder();
-
-	int number = 0;
-	while (number != -1){
-		cout << "Enter a positive number to be searched, enter -1 to exit: \n";
-		cin >> number;
-		if (root.search(number) == true) cout << "Found\n\n";
-		else cout << "Not Found\n\n";
-	}
+	// create and open output file for names
+	// print the BST of names in BREADTH to output file
+	ofstream birthdateOutputFile;
+	birthdateOutputFile.open("./sortedBirthdates.txt");
+	birthDates.printBreadthFirstTraverse(birthdateOutputFile);
+	birthdateOutputFile.close();
 
 	return 0;
 }
-
-
-		/*
-		QueueADT<string> queuedList;
-		LinkedList<double> testList;
-		QueueADT<Currency> currencyQueue;
-		testList.push_first(11);
-		testList.push_first(7);
-		testList.push_first(5);
-		testList.push_first(3);
-		testList.push_first(1);
-		testList.displayList();
-		cout << "\ninsert '2.2' at posistion 3";
-		testList.insert_node(2.2, 3);
-		cout << endl;
-		testList.displayList();
-		cout << "\ndeleting 2.2\n";
-		testList.deleteNode(2.2);
-		testList.displayList();
-		cout << "\ndeleting 11 and 1\n";
-		testList.deleteNode(11);
-		testList.deleteNode(1);
-		testList.displayList();
-		cout << "\nfirst returns: " << testList.getFirst() << endl;
-		cout << "last returns: " << testList.getLast() << endl;
-		cout << "getIndex(0) returns: " << testList.getIndex(0) << endl;
-		cout << "getIndex(1) returns: " << testList.getIndex(1) << endl;
-		cout << "getIndex(2) returns: " << testList.getIndex(2) << endl;
-		cout << "empty the list\n";
-		testList.emptyList();
-		cout << "display the list\n";
-		testList.displayList();
-		//cout << "getIndex(4) returns: " << testList.getIndex(4) << endl;  // don't go out of bounds
-		//string x;
-		//double x;
-		//testList.top(x);
-		//cout << "\n\n";
-		//testList.printLast();
-		//cout << "\n\n" << x;
-		LinkedList<string> strList;
-		strList.push_first("one");
-		strList.push_first("three");
-		strList.push_first("five");
-		strList.push_first("seven");
-		strList.push_first("eleven");
-		cout << endl << endl;
-		strList.displayList();
-		cout << endl;
-		cout << "\npop first\n";
-		strList.pop_first();
-		strList.displayList();
-		cout << endl;
-		cout << "\npop last\n";
-		strList.pop_last();
-		strList.displayList();
-		cout << endl;
-		//string y;
-		//strList.top(y);
-		cout << "getFirst() returns: " << strList.getFirst() << endl;
-		cout << "search for seven returns: " << strList.searchNodes("seven") << endl;
-		cout << "search for five returns: " << strList.searchNodes("five") << endl;
-		cout << "search for eleven returns: " << strList.searchNodes("eleven") << endl;
-		LinkedList<string> str2List;
-		cout << endl;
-		str2List.displayList();
-		cout << endl << endl << endl;
-		cout << "enqueue 'one' and 'two'" << endl;
-		queuedList.enQueue("one");
-		queuedList.enQueue("two");
-		cout << "display queue:" << endl;
-		queuedList.displayQueue();
-		cout << "\nenqueue 'three' and 'four'" << endl;
-		queuedList.enQueue("three");
-		queuedList.enQueue("four");
-		cout << "display queue:" << endl;
-		queuedList.displayQueue();
-		cout << "\nenqueue 'five'" << endl;
-		queuedList.enQueue("five");
-		cout << "display queue:" << endl;
-		queuedList.displayQueue();
-		cout << "\ndequeue one item" << endl;
-		queuedList.deQueue();
-		cout << "display queue" << endl;
-		queuedList.displayQueue();
-		cout << "\ndequeue one item" << endl;
-		queuedList.deQueue();
-		cout << "display queue" << endl;
-		queuedList.displayQueue();
-		cout << endl << "front returns: ";
-		queuedList.printFront();
-		cout << endl << "rear returns: ";
-		queuedList.printRear();
-		cout << endl;
-		Currency currency1("Dollar", "Cent", 12, 55);
-		Currency currency2("Euro", "Pence", 2, 78);
-		Currency currency3("Peso", "Cento", 5, 97);
-		Currency currency4("Ruble", "Kopecks", 43, 15);
-		currencyQueue.enQueue(currency1);
-		currencyQueue.enQueue(currency2);
-		currencyQueue.enQueue(currency3);
-		currencyQueue.enQueue(currency4);
-		currencyQueue.displayQueue();
-		//int xyz;
-		//cout << endl << xyz;
-		cout << "\n\npress <Enter> to exit the program...";
-		cin.get();
-		*/
-
-
-
-
-
-
-
